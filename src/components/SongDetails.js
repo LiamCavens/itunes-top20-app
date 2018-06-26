864956c6eeee8832371604c9c17163819e6451e3
Liam Cavens <liamcavens@googlemail.com>
2018-06-26 22:20:27 +0100

    final css

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -5,11 +5,13 @@ const SongDetails = props => (
   <div id="song-details">
     <h1 className="song-details-title">Tap Twitty Tunes</h1>
     <h2 className="song-details-header2">{props.song.fullTitle}</h2>
-    <img className="song-details-image" src={props.song.largeImage} alt="" />
-    <video controls muted src={props.song.sample} width="300" height="45" />
-    <p>Title : {props.song.name}</p>
-    <p>Artist : {props.song.artist}</p>
-    <p>Album : {props.song.album}</p>
+    <div className="deets-container">
+      <img className="song-details-image" src={props.song.largeImage} alt="" />
+      <video controls muted src={props.song.sample} width="300" height="45" />
+      <p>Title : {props.song.name}</p>
+      <p>Artist : {props.song.artist}</p>
+      <p>Album : {props.song.album}</p>
+    </div>
   </div>
 );
 
